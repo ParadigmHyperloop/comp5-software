e814ee4ad4d4a9d2e6a2c88b077cb8085d129263
Andrew Nash <33398082+andrewnash@users.noreply.github.com>
2019-10-13 16:05:21 -0230

    update extentions to .js

--- OnLand/webpack.config.js
+++ OnLand/webpack.config.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 module.exports = {
 	context: __dirname,
-	entry: './src/App.jsx',
+	entry: './src/App.js',
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'bundle.js',
@@ -15,7 +15,7 @@ module.exports = {
 	},
 	resolve: {
 		modules: ['node_modules', './src'],
-		extensions: ['.js', '.jsx'],
+		extensions: ['.js'],
 	},
 	module: {
 		rules: [

cc348ac12e8437d54e4da039a1555fbedca92eb1
Colton Smith <47944858+colton-smith@users.noreply.github.com>
2019-10-16 23:40:51 -0230

    Define I2CWrapper .h/.cpp files with initial functionality

--- OnPod/Node/Examples/I2C-Example/include/I2Cmanager.h
+++ OnPod/Node/Examples/I2C-Example/include/I2Cmanager.h
@@ -6,21 +6,42 @@ class I2C
 
     // I2C Handle Type Def structure contains the config info for the specified I2C.
     I2C_HandleTypeDef m_hi2c;
+    uint8_t* m_data;
 
     public:
 
     // Constructor
     I2C(I2C_HandleTypeDef hi2c);
-
+    
     // Getter/setter for I2C HandleTypeDef
     void setHandleTypeDef(I2C_HandleTypeDef hi2c);
     I2C_HandleTypeDef getHandleTypeDef();
 
-    // Various transmit and recieve functions(some undefined as of yet)
-    void masterTransmit(uint16_t address, uint8_t *data, uint32_t size, uint32_t timeout);    
-    void masterRecieve();
-    void slaveTransmit();
-    void slaveRecieve();
+    // Getter for data
+    uint8_t* getRecentData();
+
+    //get state, mode, and error methods
+    HAL_I2C_StateTypeDef getState();
+    HAL_I2C_ModeTypeDef getMode();
+    uint32_t getError();
+
+    /* Getter and setter for addressing mode (10 bit or 7 bit) 
+        #defined as:
+        I2C_ADDRESSINGMODE_7BIT
+        I2C_ADDRESSINGMODE_10BIT
+    */
+    void setAddressingMode(uint32_t mode);
+    uint32_t getAddressingMode();
+
+    // Master transmit and recieve functions (blocking mode)
+    HAL_StatusTypeDef masterTransmit(uint16_t address, uint8_t *data, uint32_t timeout);   
+    HAL_StatusTypeDef masterRecieve(uint16_t address, uint8_t *data, uint32_t size, uint32_t timeout);
+
+    // Config methods for digital and analog  noise filters
+    HAL_StatusTypeDef configAnalogFilter(uint32_t FilterState);
+    HAL_StatusTypeDef configDigitalFilter(uint32_t FilterState);
+   
    
 };
+
 
--- OnPod/Node/Examples/I2C-Example/src/I2Cmanager.cpp
+++ OnPod/Node/Examples/I2C-Example/src/I2Cmanager.cpp
@@ -1,46 +1,96 @@
-#include "I2Cwrapper.h"
+#include"main.h"
+#include "I2CManager.h"
 
-// constructor
+// Constructor
 I2C::I2C(I2C_HandleTypeDef hi2c)
 {
     setHandleTypeDef(hi2c);
 }
 
-// setter for I2C HandleTypeDef structure
+//setter for I2C HandleTypeDef structure
 void I2C::setHandleTypeDef(I2C_HandleTypeDef hi2c)
 {
     m_hi2c = hi2c;
 }
 
-// getter for I2C HandleTypeDef
+//getter for I2C HandleTypeDef
 I2C_HandleTypeDef I2C::getHandleTypeDef()
 {
     return m_hi2c;
 }
 
+// setter for I2C addressing mode 
+void I2C::setAddressingMode(uint32_t mode)
+{
+    m_hi2c.Init.AddressingMode = mode;
+}
+
+//getter for I2C addressing mode
+uint32_t I2C::getAddressingMode()
+{
+    return m_hi2c.Init.AddressingMode;
+}
+
+//getter for I2C state
+HAL_I2C_StateTypeDef I2C::getState()
+{
+    return(HAL_I2C_GetState(&m_hi2c));
+}
+
+//getter for I2C mode
+HAL_I2C_ModeTypeDef I2C::getMode()
+{
+    return(HAL_I2C_GetMode(&m_hi2c));
+}
+
+//getter for I2C error code
+uint32_t I2C::getError()
+{
+    return(HAL_I2C_GetError(&m_hi2c));
+}
+
+uint8_t* I2C::getRecentData()
+{
+    return m_data;
+}
+
 //Transmits in master mode an amount of data in blocking mode
 /*
     @params - address: Target device 7-bit address 
-            - *data: pointer to data buffer
+            - *data: pointer to data buffer - uint8_t
             - size: Amount of data to be sent
             - timeout: timeout duration
 */
-void I2C::masterTransmit(uint16_t address, uint8_t *data, uint32_t size, uint32_t timeout)     
+HAL_StatusTypeDef I2C::masterTransmit(uint16_t address, uint8_t *data, uint32_t timeout)     
 {
-    HAL_I2C_Master_Transmit(&m_hi2c, address, data, size, timeout);  
+
+    uint32_t size = sizeof(*data);
+    m_data = data;
+    return HAL_I2C_Master_Transmit(&m_hi2c, address, data, size, timeout);  
 }
 
-void I2C::masterRecieve()
+//Receive in master mode an amount of data in blocking mode
+HAL_StatusTypeDef I2C::masterRecieve(uint16_t address, uint8_t *data, uint32_t size, uint32_t timeout)
 {
-    //implement
+    return HAL_I2C_Master_Receive(&m_hi2c, address, data, size, timeout);
 }
 
-void I2C::slaveTransmit()
+/*  Configures analog noise filter - FilterState is the new state of 
+    analog noise filter.
+
+    #defines:
+    I2C_ANALOGFILTER_ENABLE
+    I2C_ANALOGFILTER_DISABLE
+*/
+HAL_StatusTypeDef I2C::configAnalogFilter(uint32_t FilterState)
 {
-    //implement
+    return(HAL_I2CEx_ConfigAnalogFilter(&m_hi2c, FilterState ));
 }
 
-void I2C::slaveRecieve()
+/*  Configures digital noise filter - FilterState is the coefficient of 
+    digital noise filter between 0x00 and 0x0F (0-15)
+*/
+HAL_StatusTypeDef I2C::configDigitalFilter(uint32_t FilterState)
 {
-    //implement
+    return(HAL_I2CEx_ConfigDigitalFilter(&m_hi2c, FilterState));
 }

2d5eebc19f59ce4a4ffc7d5880c79ee4c83f257a
Colton Smith <47944858+colton-smith@users.noreply.github.com>
2019-10-17 18:21:36 -0230

    Added reading/writing to and from device registers with memRead and memWrite methods

--- OnPod/Node/Examples/I2C-Example/include/I2Cmanager.h
+++ OnPod/Node/Examples/I2C-Example/include/I2Cmanager.h
@@ -4,21 +4,37 @@ class I2C
 {
     private:
 
-    // I2C Handle Type Def structure contains the config info for the specified I2C.
-    I2C_HandleTypeDef m_hi2c;
-    uint8_t* m_data;
+    // pointer to I2C Handle Type Def structure contains the config info for the specified I2C.
+    I2C_HandleTypeDef *m_hi2c;
+
+    //pointer to data buffer supplied in master transmit method
+
+    uint8_t *m_recentData; // pointer to data most recently transmitted
+    uint8_t *m_dataRecieved; // pointer to data most recently received
+    uint16_t m_dev_address; // address of device most recently transmitted to
+    uint32_t m_timeout; // timeout 
 
     public:
 
     // Constructor
-    I2C(I2C_HandleTypeDef hi2c);
+    I2C(I2C_HandleTypeDef *hi2c, uint16_t dev_address, uint32_t timeout);
     
     // Getter/setter for I2C HandleTypeDef
-    void setHandleTypeDef(I2C_HandleTypeDef hi2c);
-    I2C_HandleTypeDef getHandleTypeDef();
+    HAL_I2C_StateTypeDef setHandleTypeDef(I2C_HandleTypeDef *hi2c);
+    I2C_HandleTypeDef* getHandleTypeDef();
 
     // Getter for data
+    void setRecentData(uint8_t *pData);
     uint8_t* getRecentData();
+
+    void setRecievedData(uint8_t *pData);
+    uint8_t* getRecievedData();
+
+    void setDevAddress(uint16_t dev_address);
+    uint16_t getDevAddress();
+
+    void setTimeout(uint32_t timeout);
+    uint32_t getTimeout();
 
     //get state, mode, and error methods
     HAL_I2C_StateTypeDef getState();
@@ -34,14 +50,19 @@ class I2C
     uint32_t getAddressingMode();
 
     // Master transmit and recieve functions (blocking mode)
-    HAL_StatusTypeDef masterTransmit(uint16_t address, uint8_t *data, uint32_t timeout);   
-    HAL_StatusTypeDef masterRecieve(uint16_t address, uint8_t *data, uint32_t size, uint32_t timeout);
+    HAL_StatusTypeDef masterTransmit(uint16_t address, uint8_t *data);   
+    HAL_StatusTypeDef masterRecieve(uint16_t address, uint8_t *data);
+
+    // Reading / writing from internal register addresses. Functions for use with internal memory. 
+    HAL_StatusTypeDef memWrite(uint8_t *data, uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize);
+    HAL_StatusTypeDef memRead(uint8_t *data, uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize);
+    HAL_StatusTypeDef isDeviceReady(uint16_t devAddress, uint32_t trials = 100);
 
     // Config methods for digital and analog  noise filters
     HAL_StatusTypeDef configAnalogFilter(uint32_t FilterState);
     HAL_StatusTypeDef configDigitalFilter(uint32_t FilterState);
-   
-   
+    
+
 };
 
 

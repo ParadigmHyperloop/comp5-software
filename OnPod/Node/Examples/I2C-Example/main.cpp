a7f8c0c18038d4c87db0375379728c65f80b5a89
Colton Smith <47944858+colton-smith@users.noreply.github.com>
2019-10-17 20:25:22 -0230

    Add further testing of I2C wrapper

--- OnPod/Node/Examples/I2C-Example/main.cpp
+++ OnPod/Node/Examples/I2C-Example/main.cpp
@@ -1,17 +1,15 @@
 #include "main.h"
 #include "I2CManager.h"
 
-
 I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
+I2C_HandleTypeDef hi2c3;
 
+void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
-
-extern "C" 
-{
-    
-    void SystemClock_Config(void);
-}
+static void MX_I2C2_Init(void);
+static void MX_I2C3_Init(void);
 
 int main(void) 
 {
@@ -20,22 +18,45 @@ int main(void)
   SystemClock_Config();
   MX_GPIO_Init();
   MX_I2C1_Init();
-
-  //instantiate i2c object
-  I2C i2c1(hi2c1);
+  MX_I2C2_Init();
+  MX_I2C3_Init();
 
   //dummy values for testing on scope / with arduino
-  uint16_t address = 1;
-  uint8_t data[4] = {1,0,1,0};
-  uint32_t timeout = 100000;
-  uint8_t storedData;
+  uint16_t address1 = 8;
+  uint16_t address2 = 9;
+  uint16_t address3 = 10;
+  uint8_t data1[1] = {1};
+  uint8_t data2[1] = {0};
+  uint8_t data3[1] = {1};
+  uint32_t timeout = 100;
 
+  //variables to test member variables
+  uint8_t *classData = NULL;
+  uint16_t classAddress;
+
+  //instantiate i2c object
+  I2C i2c1(&hi2c1, timeout);
+  
   while (1)
   { 
     //transmit
-    i2c1.masterTransmit(address, data, timeout);
-    HAL_Delay(100);
-    storedData = i2c1.getRecentData()
+    for(uint8_t i = 0 ; i < 10 ; i++)
+    {
+      i2c1.masterTransmit(address1, data1);
+      classAddress = i2c1.getDevAddress();
+      classData = i2c1.getRecentData();
+      HAL_Delay(1000);
+
+      i2c1.masterTransmit(address2, data2);
+      classAddress = i2c1.getDevAddress();
+      classData = i2c1.getRecentData();
+      HAL_Delay(1000);
+
+      i2c1.masterTransmit(address3, data3);
+      classAddress = i2c1.getDevAddress();
+      classData = i2c1.getRecentData();
+      HAL_Delay(1000);
+    }
   }
 
 }
@@ -69,8 +90,11 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1|RCC_PERIPHCLK_I2C2
+                              |RCC_PERIPHCLK_I2C3;
   PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  PeriphClkInit.I2c2ClockSelection = RCC_I2C2CLKSOURCE_HSI;
+  PeriphClkInit.I2c3ClockSelection = RCC_I2C3CLKSOURCE_HSI;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
@@ -124,6 +148,98 @@ static void MX_I2C1_Init(void)
 }
 
 /**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.Timing = 0x2000090E;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter 
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter 
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
+  * @brief I2C3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C3_Init(void)
+{
+
+  /* USER CODE BEGIN I2C3_Init 0 */
+
+  /* USER CODE END I2C3_Init 0 */
+
+  /* USER CODE BEGIN I2C3_Init 1 */
+
+  /* USER CODE END I2C3_Init 1 */
+  hi2c3.Instance = I2C3;
+  hi2c3.Init.Timing = 0x2000090E;
+  hi2c3.Init.OwnAddress1 = 0;
+  hi2c3.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c3.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c3.Init.OwnAddress2 = 0;
+  hi2c3.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c3.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c3.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter 
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c3, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter 
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c3, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C3_Init 2 */
+
+  /* USER CODE END I2C3_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -134,6 +250,7 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 

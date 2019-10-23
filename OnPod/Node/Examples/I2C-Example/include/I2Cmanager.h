04e6de34c7dc49590e88d36a889787ecab352dd6
Colton Smith <47944858+colton-smith@users.noreply.github.com>
2019-10-23 15:51:18 -0230

    Remove unnecessary classes and add device address bitshift as specified in HAL docs

--- OnPod/Node/Examples/I2C-Example/include/I2Cmanager.h
+++ OnPod/Node/Examples/I2C-Example/include/I2Cmanager.h
@@ -1,64 +1,43 @@
-#include "main.h"
+#pragma once
 
-class I2C
+class I2CManager
 {
     private:
 
     // pointer to I2C Handle Type Def structure contains the config info for the specified I2C.
     I2C_HandleTypeDef *m_hi2c;
-    uint8_t *m_recentData; // pointer to data most recently transmitted
-    uint8_t *m_dataRecieved; // pointer to data most recently received
-    uint16_t m_dev_address; // address of device most recently transmitted to
-    uint32_t m_timeout; // timeout 
+    uint8_t m_dataRecieved[4]; // Data buffer for read data
+    uint32_t m_timeout; // timeout
 
     public:
 
     // Constructor
-    I2C(I2C_HandleTypeDef *hi2c, uint32_t timeout);
+    I2CManager(I2C_HandleTypeDef *hi2c);
     
-    // Getter/setter for I2C HandleTypeDef
-    HAL_I2C_StateTypeDef setHandleTypeDef(I2C_HandleTypeDef *hi2c);
-    I2C_HandleTypeDef* getHandleTypeDef();
+    // Returns a pointer to the data recieved buffer
+    uint8_t* getRecievedData() {  return m_dataRecieved; }
 
-    // Getter for data
-    void setRecentData(uint8_t *pData);
-    uint8_t* getRecentData();
+    // Get state, and error methods
+    HAL_I2C_StateTypeDef getState() { return HAL_I2C_GetState(m_hi2c); }
+    uint32_t getError() { return HAL_I2C_GetError(m_hi2c); }
 
-    void setRecievedData(uint8_t *pData);
-    uint8_t* getRecievedData();
-
-    void setDevAddress(uint16_t dev_address);
-    uint16_t getDevAddress();
-
-    void setTimeout(uint32_t timeout);
-    uint32_t getTimeout();
-
-    //get state, mode, and error methods
-    HAL_I2C_StateTypeDef getState();
-    HAL_I2C_ModeTypeDef getMode();
-    uint32_t getError();
-
-    /* Getter and setter for addressing mode (10 bit or 7 bit) 
-        #defined as:
-        I2C_ADDRESSINGMODE_7BIT
-        I2C_ADDRESSINGMODE_10BIT
-    */
-    void setAddressingMode(uint32_t mode);
-    uint32_t getAddressingMode();
+    // Getter and setter for I2C addressing mode
+    void setAddressingMode(uint32_t mode) { (*m_hi2c).Init.AddressingMode = mode; }
+    uint32_t getAddressingMode() { return (*m_hi2c).Init.AddressingMode; }
 
     // Master transmit and recieve functions (blocking mode)
-    HAL_StatusTypeDef masterTransmit(uint16_t address, uint8_t *data);   
-    HAL_StatusTypeDef masterRecieve(uint16_t address, uint8_t *data);
+    HAL_StatusTypeDef masterTransmit(uint16_t address, uint8_t *data, uint32_t timeout);   
+    HAL_StatusTypeDef masterRecieve(uint16_t address, uint32_t timeout);
 
-    // Reading / writing from internal register addresses. Functions for use with internal memory. 
-    HAL_StatusTypeDef memWrite(uint8_t *data, uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize);
-    HAL_StatusTypeDef memRead(uint8_t *data, uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize);
+    // Reading / writing from device internal memory addresses 
+    HAL_StatusTypeDef memWrite(uint8_t *data, uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize, uint32_t timeout);
+    HAL_StatusTypeDef memRead(uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize, uint32_t timeout);
     HAL_StatusTypeDef isDeviceReady(uint16_t devAddress, uint32_t trials = 100);
 
     // Config methods for digital and analog  noise filters
+    //TO BE TESTED
     HAL_StatusTypeDef configAnalogFilter(uint32_t FilterState);
     HAL_StatusTypeDef configDigitalFilter(uint32_t FilterState);
-    
 };
 
 
--- OnPod/Node/Examples/I2C-Example/src/I2Cmanager.cpp
+++ OnPod/Node/Examples/I2C-Example/src/I2Cmanager.cpp
@@ -2,15 +2,9 @@
 #include "I2CManager.h"
 
 // Constructor 
-I2C::I2C(I2C_HandleTypeDef *hi2c, uint32_t timeout)
+I2CManager::I2CManager(I2C_HandleTypeDef *hi2c)
 {
-    setHandleTypeDef(hi2c);
-    setTimeout(timeout);
-}
 
-//setter for I2C HandleTypeDef structure
-HAL_I2C_StateTypeDef I2C::setHandleTypeDef(I2C_HandleTypeDef *hi2c)
-{
     HAL_I2C_StateTypeDef state = HAL_I2C_GetState(hi2c);
 
     if(state==HAL_I2C_STATE_READY )
@@ -21,141 +15,37 @@ HAL_I2C_StateTypeDef I2C::setHandleTypeDef(I2C_HandleTypeDef *hi2c)
     {
         //implement error handling
     }
-    return state;
 }
 
-//getter for I2C HandleTypeDef
-I2C_HandleTypeDef* I2C::getHandleTypeDef()
-{
-    return m_hi2c;
-}
-
-//setter for target device address
-void I2C::setDevAddress(uint16_t devAddress)
-{
-    m_dev_address = devAddress;
-}
-
-//getter for device address
-uint16_t I2C::getDevAddress()
-{
-    return m_dev_address;
-}
-
-
-void I2C::setTimeout(uint32_t timeout)
-{
-    m_timeout = timeout;
-}
-
-uint32_t I2C::getTimeout()
-{
-    return m_timeout;
-}
-
-// setter for I2C addressing mode (7 or 10 bit addressing mode)
-void I2C::setAddressingMode(uint32_t mode)
-{
-    (*m_hi2c).Init.AddressingMode = mode;
-}
-
-//getter for I2C addressing mode (7 or 10 bit addressing mode)
-uint32_t I2C::getAddressingMode()
-{
-    return (*m_hi2c).Init.AddressingMode;
-}
-
-//getter for I2C mode
-HAL_I2C_ModeTypeDef I2C::getMode()
-{
-    return(HAL_I2C_GetMode(m_hi2c));
-}
-
-//getter for I2C state
-HAL_I2C_StateTypeDef I2C::getState()
-{
-    return(HAL_I2C_GetState(m_hi2c));
-}
-
-//getter for I2C error code
-uint32_t I2C::getError()
-{
-    return(HAL_I2C_GetError(m_hi2c));
-}
-
-//getter for the current / most recent data set
-uint8_t* I2C::getRecentData()
-{
-    return m_recentData;
-}
-
-void I2C::setRecentData(uint8_t *data)
-{
-    m_recentData = data;
-}
-
-uint8_t* I2C::getRecievedData()
-{
-    return m_dataRecieved;
-}
-
-void I2C::setRecievedData(uint8_t *data)
-{
-    m_dataRecieved = data;
-}
-
-
-
-//TRANSMISSION FUNCTIONS
-//Transmits in master mode an amount of data in blocking mode
-/*
-    @params - address: Target device 7-bit address 
-            - *data: pointer to data buffer - uint8_t
-            - size: Amount of data to be sent
-            - timeout: timeout duration
-*/
-HAL_StatusTypeDef I2C::masterTransmit(uint16_t devAddress, uint8_t *data)     
+HAL_StatusTypeDef I2CManager::masterTransmit(uint16_t devAddress, uint8_t *data, uint32_t timeout)     
 {
     uint32_t size = sizeof(*data); 
-    setRecentData(data);
-    setDevAddress(devAddress);
-    return (HAL_I2C_Master_Transmit(m_hi2c, devAddress, data, size, m_timeout));  
+    return HAL_I2C_Master_Transmit(m_hi2c, devAddress<<1, data, size, timeout);  
 }
 
-//Receive in master mode an amount of data in blocking mode using 
-HAL_StatusTypeDef I2C::masterRecieve(uint16_t devAddress, uint8_t *data)
+HAL_StatusTypeDef I2CManager::masterRecieve(uint16_t devAddress, uint32_t timeout)
 {
-    uint32_t size = sizeof(*data); 
-    setRecievedData(data);
-    setDevAddress(devAddress);
-    return HAL_I2C_Master_Receive(m_hi2c, devAddress, data, size, m_timeout);
+    uint32_t size = sizeof(*m_dataRecieved); 
+    return HAL_I2C_Master_Receive(m_hi2c, devAddress<<1, m_dataRecieved, size, timeout);
 }
 
-HAL_StatusTypeDef I2C::memWrite(uint8_t *data, uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize)
+HAL_StatusTypeDef I2CManager::memWrite(uint8_t *data, uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize, uint32_t timeout)
 {
     uint32_t size = sizeof(*data); 
-    setRecentData(data);
-    setDevAddress(devAddress);
-    return (HAL_I2C_Mem_Write(m_hi2c, devAddress, memAddress, memAddressSize, data, size, m_timeout));
-
+    return HAL_I2C_Mem_Write(m_hi2c, devAddress, memAddress, memAddressSize, data, size, timeout);
 }
 
-HAL_StatusTypeDef I2C::memRead(uint8_t *data, uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize)
+HAL_StatusTypeDef I2CManager::memRead(uint16_t devAddress, uint16_t memAddress, uint16_t memAddressSize, uint32_t timeout)
 {
-    uint32_t size = sizeof(*data); 
-    setRecievedData(data);
-    setDevAddress(devAddress);
-    return (HAL_I2C_Mem_Write(m_hi2c, devAddress, memAddress, memAddressSize, data, size, m_timeout));
+    uint32_t size = sizeof(*m_dataRecieved); 
+    return HAL_I2C_Mem_Write(m_hi2c, devAddress, memAddress, memAddressSize, m_dataRecieved, size, timeout);
 }
 
 //Checks if target device is ready for communication
-HAL_StatusTypeDef I2C::isDeviceReady(uint16_t devAddress, uint32_t trials)
+HAL_StatusTypeDef I2CManager::isDeviceReady(uint16_t devAddress, uint32_t trials)
 {
-    return (HAL_I2C_IsDeviceReady(m_hi2c, devAddress, trials, m_timeout ));
+    return HAL_I2C_IsDeviceReady(m_hi2c, devAddress, trials, m_timeout );
 }
-
-
-
 
 //CONFIGURATION FUNCTIONS
 /*  Configures analog noise filter - FilterState is the new state of 
@@ -165,15 +55,15 @@ HAL_StatusTypeDef I2C::isDeviceReady(uint16_t devAddress, uint32_t trials)
     I2C_ANALOGFILTER_ENABLE
     I2C_ANALOGFILTER_DISABLE
 */
-HAL_StatusTypeDef I2C::configAnalogFilter(uint32_t FilterState)
+HAL_StatusTypeDef I2CManager::configAnalogFilter(uint32_t FilterState)
 {
-    return(HAL_I2CEx_ConfigAnalogFilter(m_hi2c, FilterState ));
+    return HAL_I2CEx_ConfigAnalogFilter(m_hi2c, FilterState );
 }
 
 /*  Configures digital noise filter - FilterState is the coefficient of 
     digital noise filter between 0x00 and 0x0F
 */
-HAL_StatusTypeDef I2C::configDigitalFilter(uint32_t FilterState)
+HAL_StatusTypeDef I2CManager::configDigitalFilter(uint32_t FilterState)
 {
-    return(HAL_I2CEx_ConfigDigitalFilter(m_hi2c, FilterState));
+    return HAL_I2CEx_ConfigDigitalFilter(m_hi2c, FilterState);
 }

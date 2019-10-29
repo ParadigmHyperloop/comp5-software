14a37af5b0b2bf6dde691512b4ca63fc4501b684
Burke-Daniel <52763491+Burke-Daniel@users.noreply.github.com>
2019-10-29 20:06:10 -0230

    Add test functions for each method of class

--- OnPod/Node/Test/PWMManager/main.cpp
+++ OnPod/Node/Test/PWMManager/main.cpp
@@ -3,15 +3,40 @@
 
 PWMManager PWM(8000000, 0, 200, 100, htim2, TIM_CHANNEL_1);
 
-void testOne(void)
+void testSetFrequencyHz(void)
 {
-    TEST_ASSERT_EQUAL_UINT8(7, 7);
+    PWM.setFrequencyHz(300);
+    TEST_ASSERT_EQUAL_UINT8(300, PWM.getFrequencyHz());
+}
+
+void testSetPeriodMs(void)
+{
+    PWM.setPeriodMs(200);
+    TEST_ASSERT_EQUAL_UINT32(200, PWM.getPeriodMs());
+}
+
+void testSetPeriodUs(void)
+{
+    PWM.setPeriodUs(50);
+    TEST_ASSERT_EQUAL_UINT32(50, PWM.getPeriodUs());
+}
+
+void testSetPulseMs(void)
+{
+    PWM.setPulseMs(100);
+    TEST_ASSERT_EQUAL_UINT32(100, PWM.getPulseMs());
+}
+
+void testSetPulseUs(void)
+{
+    PWM.setPulseUs(25);
+    TEST_ASSERT_EQUAL_UINT32(25, PWM.getPulseUs());
 }
 
 int main(void) 
 {
     UNITY_BEGIN();
-    RUN_TEST(testOne);
+    RUN_TEST(testSetFrequencyHz);
     UNITY_END();
     return 0;
 }

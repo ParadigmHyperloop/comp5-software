7e449cf152ecbce4e9976ff717b3a1c8a08591c2
Burke-Daniel <52763491+Burke-Daniel@users.noreply.github.com>
2019-10-29 19:26:38 -0230

    Add getters for each setter method

--- OnPod/Node/Examples/PWM-Example/include/PWMManager.h
+++ OnPod/Node/Examples/PWM-Example/include/PWMManager.h
@@ -11,11 +11,17 @@ public:
     void PWMStart();
     void PWMStop();
     void setPeriodMs(uint32_t newPeriod);
+    uint32_t getPeriodMs();
     void setPeriodUs(uint32_t newPeriod);
+    uint32_t getPeriodUs();
     void setDutyCyclePercent(uint8_t newDutyCycle);
+    uint8_t getDutyCyclePercent();
     void setFrequencyHz(uint32_t newFrequency);
+    uint32_t getFrequencyHz();
     void setPulseMs(uint32_t newPulse);
+    uint32_t getPulseMs();
     void setPulseUs(uint32_t newPulse);
+    uint32_t getPulseUs();
 
 private:
     uint64_t m_clockSpeed;
--- OnPod/Node/Examples/PWM-Example/source/PWMManager.cpp
+++ OnPod/Node/Examples/PWM-Example/source/PWMManager.cpp
@@ -25,10 +25,20 @@ void PWMManager::setPulseMs(uint32_t newPulse)
     m_interface.setPulse(m_pulse);
 }
 
+uint32_t PWMManager::getPulseMs()
+{
+    return m_pulse * 1000.0;
+}
+
 void PWMManager::setPulseUs(uint32_t newPulse)
 {
     m_pulse = (newPulse / 1000000.0) * ((float)m_clockSpeed / (float)m_pulse);
     m_interface.setPulse(m_pulse);
+}
+
+uint32_t PWMManager::getPulseUs()
+{
+    return m_pulse * 1000000.0;
 }
 
 void PWMManager::setDutyCyclePercent(uint8_t newDutyCycle)
@@ -37,10 +47,20 @@ void PWMManager::setDutyCyclePercent(uint8_t newDutyCycle)
     m_interface.setPulse(m_pulse);
 }
 
+uint8_t PWMManager::getDutyCyclePercent()
+{
+    return ((float)m_pulse / (float)m_period) * 100.0;
+}
+
 void PWMManager::setPeriodMs(uint32_t newPeriod)
 {
     m_period = (newPeriod / 1000.0) * ((float)m_clockSpeed / (float)m_preScaler);
     m_interface.setPeriod(m_period);
+}
+
+uint32_t PWMManager::getPeriodMs()
+{
+    return m_period * 1000.0;
 }
 
 void PWMManager::setPeriodUs(uint32_t newPeriod)
@@ -49,8 +69,18 @@ void PWMManager::setPeriodUs(uint32_t newPeriod)
     m_interface.setPeriod(m_period);
 }
 
+uint32_t PWMManager::getPeriodUs()
+{
+    return m_period * 1000000.0;
+}
+
 void PWMManager::setFrequencyHz(uint32_t newFrequency)
 {
     m_period = (1.0 / newFrequency) * ((float)m_clockSpeed / (float)m_preScaler);
     m_interface.setPeriod(m_period);
+}
+
+uint32_t PWMManager::getFrequencyHz()
+{
+    return 1.0 / m_period;
 }

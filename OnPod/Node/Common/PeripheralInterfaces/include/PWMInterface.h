0f742dba1620a5967561f44d9a0225ab4d902701
Burke-Daniel <52763491+Burke-Daniel@users.noreply.github.com>
2019-10-31 09:50:25 -0230

    Add constructor

--- OnPod/Node/Common/PeripheralInterfaces/include/PWMInterface.h
+++ OnPod/Node/Common/PeripheralInterfaces/include/PWMInterface.h
@@ -6,20 +6,20 @@
 class PWMInterface
 {
 public:
-    void PWMStart();
+    PWMInterface(uint64_t clockSpeed, uint32_t preScaler, uint32_t period,
+    uint32_t pulse, TIM_HandleTypeDef timer, uint32_t channel);
+    void PWMStart(TIM_HandleTypeDef &timer);
     void PWMStop();
     void setPeriod(uint32_t period);
     void setPulse(uint32_t pulse);
-    void setParameters(uint64_t clockSpeed, uint32_t preScaler, 
-    uint32_t period, uint32_t pulse, TIM_HandleTypeDef timer, uint32_t channel);
 
 private:
     uint64_t m_clockSpeed;
     uint32_t m_preScaler;
     uint32_t m_period;
     uint32_t m_pulse;
-    TIM_HandleTypeDef m_timer;
     uint32_t m_channel;
+    TIM_HandleTypeDef m_timer;
 };
 
 
--- OnPod/Node/Common/PeripheralInterfaces/src/PWMInterface.cpp
+++ OnPod/Node/Common/PeripheralInterfaces/src/PWMInterface.cpp
@@ -1,7 +1,14 @@
 #include "PWMInterface.h"
 
-void PWMInterface::PWMStart()
+PWMInterface::PWMInterface(uint64_t clockSpeed, uint32_t preScaler, uint32_t period,
+uint32_t pulse, TIM_HandleTypeDef timer, uint32_t channel) : 
+m_clockSpeed(clockSpeed), m_preScaler(preScaler), m_period(period),
+m_pulse(pulse), m_timer(timer), m_channel(channel)
+{}
+
+void PWMInterface::PWMStart(TIM_HandleTypeDef &timer)
 {
+    m_timer = timer;
     HAL_TIM_PWM_Start(&m_timer, m_channel);
     __HAL_TIM_SET_PRESCALER(&m_timer, m_preScaler);
     __HAL_TIM_SET_AUTORELOAD(&m_timer, m_period);
@@ -23,15 +30,4 @@ void PWMInterface::setPulse(uint32_t pulse)
 {
     m_pulse = pulse;
     __HAL_TIM_SET_COMPARE(&m_timer, m_channel, m_pulse);    
-}
-
-void PWMInterface::setParameters(uint64_t clockSpeed, uint32_t preScaler, 
-uint32_t period, uint32_t pulse, TIM_HandleTypeDef timer, uint32_t channel)
-{
-    m_clockSpeed = clockSpeed;
-    m_preScaler = preScaler;
-    m_period = period;
-    m_pulse = pulse;
-    m_timer = timer;
-    m_channel = channel;
 }

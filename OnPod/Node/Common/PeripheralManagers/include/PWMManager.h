d01a0d46760d3ae8f8736a6a2adddec34c3af86c
Burke-Daniel <52763491+Burke-Daniel@users.noreply.github.com>
2019-11-02 15:06:56 -0230

    Add getters for period and pulse in ms and us

--- OnPod/Node/Common/PeripheralManagers/include/PWMManager.h
+++ OnPod/Node/Common/PeripheralManagers/include/PWMManager.h
@@ -1,5 +1,6 @@
-#include "main.h"
+#include <stdint.h>
 #include "PWMInterface.h"
+#include "mock_PWMInterface.h"
 
 #ifndef PWMMANAGER_H
 #define PWMMANAGER_H
@@ -7,24 +8,31 @@
 class PWMManager
 {
 public:
-    PWMManager(uint64_t clockSpeed, uint32_t preScaler, uint32_t period, uint32_t pulse, TIM_HandleTypeDef &tim, uint32_t channel);
+    PWMManager(uint64_t clockSpeed, uint32_t preScaler, uint32_t period, 
+    uint32_t pulse, PWMInterface interface);
     void PWMStart();
     void PWMStop();
     void setPeriodMs(uint32_t newPeriod);
+    uint32_t getPeriodMs();
     void setPeriodUs(uint32_t newPeriod);
+    uint32_t getPeriodUs();
     void setDutyCyclePercent(uint8_t newDutyCycle);
+    uint8_t getDutyCyclePercent();
     void setFrequencyHz(uint32_t newFrequency);
+    uint32_t getFrequencyHz();
     void setPulseMs(uint32_t newPulse);
+    uint32_t getPulseMs();
     void setPulseUs(uint32_t newPulse);
+    uint32_t getPulseUs();
+    void updateInterface();
 
 private:
     uint64_t m_clockSpeed;
     uint32_t m_preScaler;
     uint32_t m_period;
-    uint32_t m_pulse;            
-    TIM_HandleTypeDef m_timer;   
-    uint32_t m_channel;         
+    uint32_t m_pulse;                   
     PWMInterface m_interface;
+    mock_PWMInterface m_mockInterface;
 };
 
 #endif

69ba52136a228c89422baac8cd48d66f378e9cf3
Burke-Daniel <52763491+Burke-Daniel@users.noreply.github.com>
2019-11-10 17:06:33 -0330

    Create SPI Interface header and begin definitions

---
+++ OnPod/Node/Common/PeripheralInterfaces/include/SPIInterface.h
@@ -0,0 +1,29 @@
+#ifndef NODE_SPIINTERFACE_H
+#define NODE_SPIINTERFACE_H
+
+#include "stm32f3xx_hal.h"
+
+class SPIInterface
+{
+private:
+
+    SPI_HandleTypeDef *m_hspi;
+    uint8_t m_dataReceived[8];
+    uint8_t m_bufferlength = 8;
+
+public:
+
+    // Constructor
+    SPIInterface(SPI_HandleTypeDef *hspi);
+
+    // Returns a pointer to the data recieved buffer
+    uint8_t* getReceivedData() { return m_dataReceived; }
+
+    // Get state, and error methods
+    HAL_SPI_StateTypeDef getState() { return HAL_SPI_GetState(m_hspi); }
+    uint32_t getError() { return HAL_SPI_GetError(m_hspi); }
+
+    // Master transmit and receive functions
+
+};
+#endif

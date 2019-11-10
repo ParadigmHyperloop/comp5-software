973919c414639536021cf817461ab74f72d146f8
Shane Williams <shanethomaswilliams@gmail.com>
2019-11-09 23:26:47 -0330

    Add more descriptive comments to EventScheduler class

--- OnPod/Node/Common/EventScheduler/include/event_scheduler.h
+++ OnPod/Node/Common/EventScheduler/include/event_scheduler.h
@@ -4,34 +4,38 @@
 #include <stdint.h>
 #include "event.h"
 
-// Ranges from 0-127. -1 indicates error in finding free ID
-typedef int8_t eventId_t;
-typedef uint32_t timeMs_t;
+typedef int8_t eventId_t;  // Should range from 0-127. -1 indicates error in finding free ID
+typedef uint32_t timeMs_t; // Time in ms
 
 class EventScheduler
 {
     private:
+        // Number of events that can be active at once
         static const uint8_t m_MAX_NUM_EVENTS = 50;
+        // Array of Event objects
         Event m_events[m_MAX_NUM_EVENTS];
-
+        
+        // Returns an unassigned Event object in array, or -1 if none are free
         eventId_t findFreeEvent(void); 
         
     public:
+        // Check if any callbacks must be called and execute them.
+        // If the repeat limit is reached, reset the event object.
         void updateEvents(timeMs_t current_time);
 
         void startEvent(eventId_t event_id);
         void stopEvent(eventId_t event_id);
 
-        // Call `callback` every `delay_ms` milliseconds, `repeat_count` times
-        // Returns index of event in `m_event_list` or -1 if `m_event_list` is full
+        // Call `callback` every `delay_ms` milliseconds, `repeat_count` times.
+        // Returns index of event in `m_event_list` or -1 if `m_event_list` is full.
         eventId_t callFunctionEvery(timeMs_t delay_ms, void (*callback)(void), 
                                     timeMs_t current_time, uint32_t repeat_count);
-        // Call `callback` every `delay_ms` milliseconds, forever
-        // Returns index of event in `m_event_list` or -1 if `m_event_list` is full
+        // Call `callback` every `delay_ms` milliseconds, forever.
+        // Returns index of event in `m_event_list` or -1 if `m_event_list` is full.
         eventId_t callFunctionEvery(timeMs_t delay_ms, void (*callback)(void),
                                     timeMs_t current_time);
-        // Call `callback` after `delay_ms` milliseconds
-        // Returns index of event in `m_event_list` or -1 if `m_event_list` is full
+        // Call `callback` after `delay_ms` milliseconds.
+        // Returns index of event in `m_event_list` or -1 if `m_event_list` is full.
         eventId_t callFunctionAfter(timeMs_t delay_ms, void (*callback)(void),
                                     timeMs_t current_time);
 };
--- OnPod/Node/Common/EventScheduler/src/event_scheduler.cpp
+++ OnPod/Node/Common/EventScheduler/src/event_scheduler.cpp
@@ -2,20 +2,25 @@
 
 void EventScheduler::updateEvents(timeMs_t current_time)
 {
+    // Iterate through m_events array
     for (eventId_t id = 0; id < m_MAX_NUM_EVENTS; id++)
     {
+        // Ignore stopped or unused event objects
         if (m_events[id].getType() == EventType::UNASSIGNED || 
             m_events[id].getState() == EventState::STOPPED)
         {
             continue;
         }
+        // If the event's delay has passed
         if ((current_time - m_events[id].getLastTimeCalledMs() >= m_events[id].getDelayMs()))
         {
             m_events[id].callCallback();
             m_events[id].setLastTimeCalledMs(current_time);
+            // If the event only runs a certain number of times
             if (m_events[id].getType() == EventType::FINITE)
             {
                 m_events[id].decrementNumCallsLeft();
+                // Unassign/reset event objects that are finished
                 if (m_events[id].getNumCallsLeft() == 0)
                 {
                     m_events[id].cleanup();

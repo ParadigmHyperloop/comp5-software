fb6a92385e661c72aaebc4530398eee828043de9
Shane Williams <shanethomaswilliams@gmail.com>
2019-11-09 21:26:24 -0330

    Add untested Event class

---
+++ OnPod/Node/Common/EventScheduler/include/event.h
@@ -0,0 +1,52 @@
+#ifndef NODE_EVENT_H
+#define NODE_EVENT_H
+
+#include <stdint.h>
+
+typedef uint32_t timeMs_t;
+
+enum class EventType
+{
+    UNASSIGNED,
+    FINITE,     // Repeat X number of times
+    FOREVER,    // Repeat forever
+};
+
+enum class EventState
+{
+    STARTED,
+    STOPPED,
+};
+
+class Event
+{
+    private:
+        EventType m_type = EventType::UNASSIGNED;
+        EventState m_state = EventState::STOPPED;
+        void (*m_callback)(void) = nullptr;
+        timeMs_t m_delay_ms = 0;
+        timeMs_t m_last_time_called_ms = 0;
+        uint32_t m_num_calls_left = 0;
+
+    public:
+        EventType getType(void);
+        EventState getState(void);
+        timeMs_t getDelayMs(void);
+        timeMs_t getLastTimeCalledMs(void);
+        uint32_t getNumCallsLeft(void);
+
+        void setType(EventType type);
+        void setState(EventState state);
+        void setDelayMs(timeMs_t delay_ms);
+        void setLastTimeCalledMs(timeMs_t time_ms);
+        void setCallback(void (*callback)(void));
+        void setNumCallsLeft(uint32_t num_calls);
+
+        void decrementNumCallsLeft(void);
+
+        void callCallback(void);
+        // Clear all event attributes to prepare for reassignment
+        void cleanup(void);
+};
+
+#endif
---
+++ OnPod/Node/Common/EventScheduler/src/event.cpp
@@ -0,0 +1,74 @@
+#include "event.h"
+
+EventType Event::getType(void)
+{
+    return m_type;
+}
+
+EventState Event::getState(void)
+{
+    return m_state;
+}
+
+uint32_t Event::getDelayMs(void)
+{
+    return m_delay_ms;
+}
+
+uint32_t Event::getLastTimeCalledMs(void)
+{
+    return m_last_time_called_ms;
+}
+
+uint32_t Event::getNumCallsLeft(void)
+{
+    return m_num_calls_left;
+}
+
+
+void Event::setType(EventType type)
+{
+    m_type = type;
+}
+
+void Event::setState(EventState state)
+{
+    m_state = state;
+}
+
+void Event::setDelayMs(timeMs_t delay_ms)
+{
+    m_delay_ms = delay_ms;
+}
+
+void Event::setLastTimeCalledMs(timeMs_t time_ms)
+{
+    m_last_time_called_ms = time_ms;
+}
+
+void Event::setCallback(void (*callback)(void))
+{
+    m_callback = callback;
+}
+
+
+void Event::decrementNumCallsLeft(void)
+{
+    m_num_calls_left--;
+}
+
+
+void Event::callCallback(void)
+{
+    m_callback();
+}
+
+void Event::cleanup(void)
+{
+    m_type = EventType::UNASSIGNED;
+    m_state = EventState::STOPPED;
+    m_callback = nullptr;
+    m_delay_ms = 0;
+    m_last_time_called_ms = 0;
+    m_num_calls_left = 0;
+}

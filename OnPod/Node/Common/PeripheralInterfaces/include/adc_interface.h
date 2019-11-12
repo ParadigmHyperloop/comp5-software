1c00f256ca27fce29ce09f1425d5a24719c4ec20
Colton Smith <47944858+colton-smith@users.noreply.github.com>
2019-11-11 22:02:55 -0330

    Create initial adc interface class

--- OnPod/Node/Common/PeripheralInterfaces/include/adc_interface.h
+++ OnPod/Node/Common/PeripheralInterfaces/include/adc_interface.h
@@ -1,8 +1,30 @@
+#include "stm32f3xx_hal.h"
+#include "stdint.h"
 
 class ADCInterface
 {
-private:
-
 public:
-    ADCInterface();
+    ADCInterface(ADC_HandleTypeDef* hadc) : m_hadc(hadc);
+   
+    // Activate ADC
+    HAL_StatusTypeDef adcStart();
+
+    // Deactivate ADC
+    HAL_StatusTypeDef adcStop();
+
+    // Poll for conversion, will return HAL_OK if conversion is performed
+    HAL_StatusTypeDef pollForConversion(uint32_t timeout);
+
+    // Returns the value of the conversion. To be called after polling returns.
+    uint32_t getValue();
+
+    // Returns the current ADC error state
+    uint32_t getError();
+
+    // Set the channel to be read 
+    HAL_StatusTypeDef configChannel(ADC_ChannelConfTypeDef* config);
+
+private:
+    // Adc handle, possible values hadc1 - hadc3
+    ADC_HandleTypeDef* m_hadc;
 };
--- OnPod/Node/Common/PeripheralInterfaces/src/adc_interface.cpp
+++ OnPod/Node/Common/PeripheralInterfaces/src/adc_interface.cpp
@@ -1,7 +1,31 @@
 #include "adc_interface.h"
 
-ADCInterface::ADCInterface()
+HAL_StatusTypeDef ADCInterface::adcStart()
 {
-    
+   return  HAL_ADC_Start(m_hadc);
+}
 
+HAL_StatusTypeDef ADCInterface::adcStop()
+{
+    return HAL_ADC_Stop(m_hadc);
+}
+
+HAL_StatusTypeDef ADCInterface::pollForConversion(uint32_t timeout)
+{
+    return HAL_ADC_PollForConversion(m_hadc, timeout);
+}
+
+uint32_t ADCInterface::getValue()
+{
+    return HAL_ADC_GetValue(m_hadc);
+}
+
+uint32_t ADCInterface::getError()
+{
+    return HAL_ADC_GetError(m_hadc);
+}
+
+HAL_StatusTypeDef ADCInterface::configChannel(ADC_ChannelConfTypeDef* config)
+{
+    return HAL_ADC_ConfigChannel(m_hadc, config);
 }

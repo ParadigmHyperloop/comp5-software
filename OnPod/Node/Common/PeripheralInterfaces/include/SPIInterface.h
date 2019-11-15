280254ef031803f1ef859076cbd7fd7e8156021c
Burke-Daniel <52763491+Burke-Daniel@users.noreply.github.com>
2019-11-15 10:04:39 -0330

    Remove unnecessary methods from class

--- OnPod/Node/Common/PeripheralInterfaces/include/SPIInterface.h
+++ OnPod/Node/Common/PeripheralInterfaces/include/SPIInterface.h
@@ -9,7 +9,6 @@ private:
 
     SPI_HandleTypeDef *m_hspi;
     uint8_t m_dataReceived[8];
-    uint8_t m_bufferlength = 8;
 
 public:
 
@@ -24,17 +23,11 @@ public:
     uint32_t getError() { return HAL_SPI_GetError(m_hspi); }
 
     // Master transmit and receive functions
-    HAL_StatusTypeDef Transmit(uint16_t address, uint8_t* data, uint32_t timeout, uint8_t size);
-    HAL_StatusTypeDef Receive(uint16_t address, uint32_t timeout);
+    HAL_StatusTypeDef Transmit(uint8_t* data, uint32_t timeout, uint8_t size);
+    HAL_StatusTypeDef Receive(uint32_t timeout, uint8_t size);
 
-    // Reading / Writing
-    HAL_StatusTypeDef memWrite(uint8_t* data, uint16_t devAddress, uint16_t memAddress, 
-                                uint16_t memAddressSize, uint8_t size, uint32_t timeout);
 
-    HAL_StatusTypeDef memRead(uint16_t devAddress, uint16_t memAddress, 
-                                uint16_t memAddressSize, uint32_t timeout);
-
-    HAL_StatusTypeDef isDeviceReady(uint16_t devAddress, uint32_t trials = 100, uint32_t timeout);
+    HAL_StatusTypeDef isDeviceReady(uint16_t devAddress, uint32_t trials, uint32_t timeout);
 
 };
 #endif
--- OnPod/Node/Common/PeripheralInterfaces/src/SPIInterface.cpp
+++ OnPod/Node/Common/PeripheralInterfaces/src/SPIInterface.cpp
@@ -2,12 +2,13 @@
 
 SPIInterface::SPIInterface(SPI_HandleTypeDef *hspi) : m_hspi(hspi){}
 
-HAL_StatusTypeDef SPIInterface::Transmit(uint16_t address, uint8_t* data, uint32_t timeout, uint8_t size)
+HAL_StatusTypeDef SPIInterface::Transmit(uint8_t* data, uint32_t timeout, uint8_t size)
 {
     return HAL_SPI_Transmit(m_hspi, data, size, timeout);
 }
 
-HAL_StatusTypeDef SPIInterface::Receive(uint16_t address, uint32_t timeout)
+HAL_StatusTypeDef SPIInterface::Receive(uint16_t size, uint32_t timeout)
 {
-    return HAL_SPI_Receive(m_hspi, m_dataReceived, m_bufferlength, timeout);
+    return HAL_SPI_Receive(m_hspi, m_dataReceived, size, timeout);
 }
+

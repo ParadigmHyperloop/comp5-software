a350ef7e6963a7f94c5249aeebf045590ac5cbb0
Shane Williams <shanethomaswilliams@gmail.com>
2019-11-15 22:20:56 -0330

    Move implementation of `Event` methods to header

--- OnPod/Node/Common/EventScheduler/include/event.h
+++ OnPod/Node/Common/EventScheduler/include/event.h
@@ -30,23 +30,27 @@ class Event
 
     public:
         // Getters
-        EventType getType(void);
-        EventState getState(void);
-        timeMs_t getDelayMs(void);
-        timeMs_t getLastTimeCalledMs(void);
-        uint32_t getNumCallsLeft(void);
+        EventType getType(void) { return m_type; };
+        EventState getState(void) { return m_state; };
+        timeMs_t getDelayMs(void) { return m_delay_ms; };
+        timeMs_t getLastTimeCalledMs(void) { return m_last_time_called_ms; };
+        uint32_t getNumCallsLeft(void) { return m_num_calls_left; };
 
         // Setters
-        void setType(EventType type);
-        void setState(EventState state);
-        void setDelayMs(timeMs_t delay_ms);
-        void setLastTimeCalledMs(timeMs_t time_ms);
-        void setCallback(void (*callback)(void));
-        void setNumCallsLeft(uint32_t num_calls);
+        void setType(EventType type) { m_type = type; };
+        void setState(EventState state) { m_state = state; };
+        void setDelayMs(timeMs_t delay_ms) { m_delay_ms = delay_ms; };
+        void setLastTimeCalledMs(timeMs_t time_ms) { 
+            m_last_time_called_ms = time_ms; 
+        };
+        void setCallback(void (*callback)(void)) { m_callback = callback; };
+        void setNumCallsLeft(uint32_t num_calls) { 
+            m_num_calls_left = num_calls; 
+        };
 
         // Take away one from the event's `m_num_calls_left`
-        void decrementNumCallsLeft(void);
-        void callCallback(void);
+        void decrementNumCallsLeft(void) { m_num_calls_left--; };
+        void callCallback(void) { m_callback(); };
         // Reset event attributes to prepare for reassignment
         void cleanup(void);
 };

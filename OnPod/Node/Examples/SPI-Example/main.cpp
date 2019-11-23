c8364d3b65137869bb0bebc4035c843e0a6cc358
Burke-Daniel <52763491+Burke-Daniel@users.noreply.github.com>
2019-11-22 21:56:16 -0330

    update main to test SPI

--- OnPod/Node/Examples/SPI-Example/main.cpp
+++ OnPod/Node/Examples/SPI-Example/main.cpp
@@ -1,4 +1,7 @@
 #include "main.h"
+#include "gpio.h"
+#include "spi.h"
+#include "SPIInterface.h"
 #include "stm32f3xx_hal.h"
 
 extern "C" 
@@ -10,6 +13,23 @@ int main(void)
 {
     HAL_Init();
     SystemClock_Config();
+    MX_GPIO_Init();
+    HAL_SPI_Init(&hspi3);
+
+    SPIInterface spi(&hspi3);
+    uint8_t data = 0x99;
+
+    while(1)
+    {
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);
+        spi.Transmit((uint8_t *) data, sizeof(data), HAL_MAX_DELAY);
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
+        HAL_Delay(1000);
+    }
+
+    
+    
+
 
 
 
